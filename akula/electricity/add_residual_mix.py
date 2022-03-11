8b28effd1bbcfb2e6feedc71a2d75ed6d3314cfc
Chris Mutel <cmutel@gmail.com>
2022-03-11 17:11:33 +0100

    Add code for electricity datapackages

---
+++ akula/electricity/add_residual_mix.py
@@ -0,0 +1,66 @@
+import bw2data as bd
+from bw2data.backends.schema import ActivityDataset as AD
+
+
+def add_swiss_residual_mix():
+    if "Swiss residual electricity mix" in bd.databases:
+        return
+
+    # From noteboook
+    switzerland_residual = {
+        "electricity production, hydro, reservoir, alpine region": 0.2814150228066876,
+        "electricity production, hydro, run-of-river": 0.636056236216345,
+        "heat and power co-generation, wood chips, 6667 kW, state-of-the-art 2014": 0.012048389472504549,
+        "heat and power co-generation, biogas, gas engine": 0.059773867534434144,
+        "heat and power co-generation, natural gas, 500kW electrical, lean burn": 0.006612375072688834,
+        "electricity production, wind, >3MW turbine, onshore": 0.0010024269784498687,
+        "electricity production, wind, 1-3MW turbine, onshore": 0.0026554668750543753,
+        "electricity production, wind, <1MW turbine, onshore": 0.00043621504383564323,
+    }
+
+    bd.projects.set_current("GSA for archetypes")
+
+    sr = bd.Database("Swiss residual electricity mix")
+    sr.register()
+
+    # Create `ActivityDataset` as this is the only way to specify the `id`.
+    AD.create(
+        id=100000,
+        code="CH-residual",
+        database="Swiss residual electricity mix",
+        location="CH",
+        name="Swiss residual electricity mix",
+        product="electricity, high voltage",
+        type="process",
+        data=dict(
+            unit="kilowatt_hour",
+            comment="Difference between generation fractions for SwissGrid and ENTSO",
+            location="CH",
+            name="Swiss residual electricity mix",
+            reference_product="electricity, high voltage",
+        ),
+    )
+
+    act = bd.get_activity(id=100000)
+    act.new_exchange(input=act, type="production", amount=1).save()
+
+    act_mapping = {
+        act: switzerland_residual[act["name"]]
+        for act in bd.Database("ecoinvent 3.8 cutoff")
+        if act["location"] == "CH"
+        and act["unit"] == "kilowatt hour"
+        and act["name"] in switzerland_residual
+    }
+    assert len(act_mapping) == len(switzerland_residual)
+
+    for key, value in act_mapping.items():
+        act.new_exchange(input=key, type="technosphere", amount=value).save()
+
+    sr.process()
+
+    bd.databases["ecoinvent 3.8 cutoff"]['depends'].append("Swiss residual electricity mix")
+    bd.databases.flush()
+
+
+if __name__ == "__main__":
+    add_swiss_residual_mix()
---
+++ akula/electricity/config.py
@@ -0,0 +1,88 @@
+ENTSO_MAPPING = {
+    "Biomass": [
+        "heat and power co-generation, wood chips, 6667 kW",
+        "heat and power co-generation, wood chips, 6667 kW, state-of-the-art 2014",
+        "heat and power co-generation, biogas, gas engine",
+    ],
+    "Fossil Brown coal/Lignite": [
+        "electricity production, lignite",
+        "heat and power co-generation, lignite",
+    ],
+    "Fossil Coal-derived gas": [
+        "treatment of coal gas, in power plant",
+    ],
+    "Fossil Gas": [
+        "electricity production, natural gas, combined cycle power plant",
+        "electricity production, natural gas, conventional power plant",
+        "heat and power co-generation, natural gas, 500kW electrical, lean burn",
+        "heat and power co-generation, natural gas, combined cycle power plant, 400MW electrical",
+        "heat and power co-generation, natural gas, conventional power plant, 100MW electrical",
+    ],
+    "Fossil Hard coal": [
+        "electricity production, hard coal",
+        "heat and power co-generation, hard coal",
+    ],
+    "Fossil Oil": [
+        "electricity production, oil",
+        "heat and power co-generation, oil",
+    ],
+    "Fossil Oil shale": [],
+    "Fossil Peat": [
+        "electricity production, peat",
+    ],
+    "Geothermal": [
+        "electricity production, deep geothermal",
+    ],
+    # This will be a bit funky; could consider charging with average of last 48 hours?
+    "Hydro Pumped Storage": [
+        "electricity production, hydro, pumped storage",
+    ],
+    "Hydro Run-of-river and poundage": [
+        "electricity production, hydro, run-of-river",
+    ],
+    "Hydro Water Reservoir": [
+        "electricity production, hydro, reservoir, alpine region",
+        "electricity production, hydro, reservoir, non-alpine region",
+    ],
+    "Nuclear": [
+        "electricity production, nuclear, boiling water reactor",
+        "electricity production, nuclear, pressure water reactor",
+        "electricity production, nuclear, pressure water reactor, heavy water moderated",
+    ],
+    "Other": [
+        "treatment of blast furnace gas, in power plant",
+    ],
+    "Other renewable": [],
+    "Solar": [
+        "electricity production, photovoltaic, 3kWp facade installation, multi-Si, laminated, integrated",
+        "electricity production, photovoltaic, 3kWp facade installation, multi-Si, panel, mounted",
+        "electricity production, photovoltaic, 3kWp facade installation, single-Si, laminated, integrated",
+        "electricity production, photovoltaic, 3kWp facade installation, single-Si, panel, mounted",
+        "electricity production, photovoltaic, 3kWp flat-roof installation, multi-Si",
+        "electricity production, photovoltaic, 3kWp flat-roof installation, single-Si",
+        "electricity production, photovoltaic, 3kWp slanted-roof installation, CIS, panel, mounted",
+        "electricity production, photovoltaic, 3kWp slanted-roof installation, CdTe, laminated, integrated",
+        "electricity production, photovoltaic, 3kWp slanted-roof installation, a-Si, laminated, integrated",
+        "electricity production, photovoltaic, 3kWp slanted-roof installation, a-Si, panel, mounted",
+        "electricity production, photovoltaic, 3kWp slanted-roof installation, multi-Si, laminated, integrated",
+        "electricity production, photovoltaic, 3kWp slanted-roof installation, multi-Si, panel, mounted",
+        "electricity production, photovoltaic, 3kWp slanted-roof installation, ribbon-Si, laminated, integrated",
+        "electricity production, photovoltaic, 3kWp slanted-roof installation, ribbon-Si, panel, mounted",
+        "electricity production, photovoltaic, 3kWp slanted-roof installation, single-Si, laminated, integrated",
+        "electricity production, photovoltaic, 3kWp slanted-roof installation, single-Si, panel, mounted",
+        "electricity production, photovoltaic, 570kWp open ground installation, multi-Si",
+        "electricity production, solar thermal parabolic trough, 50 MW",
+        "electricity production, solar tower power plant, 20 MW",
+    ],
+    "Waste": [
+        "electricity, from municipal waste incineration to generic market for electricity, medium voltage",
+    ],
+    "Wind Offshore": [
+        "electricity production, wind, 1-3MW turbine, offshore",
+    ],
+    "Wind Onshore": [
+        "electricity production, wind, 1-3MW turbine, onshore",
+        "electricity production, wind, <1MW turbine, onshore",
+        "electricity production, wind, >3MW turbine, onshore",
+    ],
+}
